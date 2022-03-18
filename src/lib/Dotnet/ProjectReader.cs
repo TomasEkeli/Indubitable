03dcef5f0aef516a354dfa2231f157611c8c719f
Tomas Ekeli <tomas.ekeli@gmail.com>
2022-03-19 00:28:11 +0100

    don't die if the project file isn't xml

--- src/lib/Dotnet/ProjectReader.cs
+++ src/lib/Dotnet/ProjectReader.cs
@@ -1,4 +1,5 @@
-﻿using System.Xml.Linq;
+﻿using System.Xml;
+using System.Xml.Linq;
 
 namespace Dotnet;
 public class ProjectReader
@@ -10,17 +11,24 @@ public class ProjectReader
             return Enumerable.Empty<Dependency>();
         }
 
-        var xml = XDocument.Parse(projectFileContents);
+        try
+        {
+            var xml = XDocument.Parse(projectFileContents);
 
-        var packageReferences = xml.Descendants("PackageReference");
+            var packageReferences = xml.Descendants("PackageReference");
 
-        return packageReferences
-            .Where(x => x.Attribute("Include") != null && x.Attribute("Version") != null)
-            .Select(packageReference =>
-                new Dependency(
-                    packageReference.Attribute("Include")!.Value,
-                    packageReference.Attribute("Version")!.Value
-                )
-            );
+            return packageReferences
+                .Where(x => x.Attribute("Include") != null && x.Attribute("Version") != null)
+                .Select(packageReference =>
+                    new Dependency(
+                        packageReference.Attribute("Include")!.Value,
+                        packageReference.Attribute("Version")!.Value
+                    )
+                );
+        }
+        catch (XmlException)
+        {
+            return Enumerable.Empty<Dependency>();
+        }
     }
 }
